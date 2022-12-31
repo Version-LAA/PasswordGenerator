3fb66ab53d357ff0196e923be1b879ab6009b208
LatoyaJA <53385426+Version-LAA@users.noreply.github.com>
2022-12-31 11:27:25 -0500

    added button click functionality

--- src/script.js
+++ src/script.js
@@ -25,39 +25,97 @@ const alphabet = [
   "x",
   "y",
   "z",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "+",
+  "?",
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
 ];
 const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?"];
 
-let randomNum = Math.round(Math.random() * 26);
+let randomNum = Math.round(Math.random() * 48);
 const defaultCharLen = document.getElementById("exampleRadios1").checked;
 const twelveCharLen = document.getElementById("exampleRadios2").checked;
 const fifteenCharLen = document.getElementById("exampleRadios3").checked;
 let passwordOutput = document.querySelector(".password-output");
-
-console.log(randomNum);
+let numOfUpperCase = Math.round(Math.random() * 3);
+let numOfSymbols = Math.round(Math.random() * 3);
+let numbers = Math.round(Math.random() * 2);
+let passWordButton = document.getElementById("generateButton");
 
 // function to generate length of string
 let generateLength = function (num) {
-  const stringPosition = [];
+  let totalChar = num;
+  let upperCase = numOfUpperCase;
+  let symbols = numOfSymbols;
+  totalChar -= upperCase + symbols;
+
+  console.log(totalChar);
+  console.log(upperCase);
+  console.log(symbols);
+  console.log(totalChar);
+
+  let stringPosition = [];
+  let x;
   for (i = 0; i < num; i++) {
-    stringPosition.push(Math.round(Math.random() * 25));
+    x = Math.round(Math.random() * 47);
+    stringPosition.push(x);
+    // console.log(randomNum);
+    // stringPosition.push(randomNum);
   }
+  console.log(stringPosition);
   return stringPosition;
 };
 
 //select password length
-function lengthCheck() {
+function generatePassword() {
   let pass = "";
+  let stringLocation;
   if (defaultCharLen) {
-    let stringLocation = generateLength(15);
+    stringLocation = generateLength(10);
     console.log(stringLocation);
     for (i = 0; i < stringLocation.length; i++) {
-      console.log(pass);
       pass += alphabet[stringLocation[i]];
     }
-    let len = pass.length;
-    return `${pass}, ${len}`;
+    passwordOutput.innerText = pass;
+    // let len = pass.length;
+    // return `${pass}, ${len}`;
+  } else if (twelveCharLen) {
+    stringLocation = generateLength(12);
+    console.log(stringLocation);
+    for (i = 0; i < stringLocation.length; i++) {
+      pass += alphabet[stringLocation[i]];
+    }
+    passwordOutput.innerText = pass;
+    // let len = pass.length;
+    // return `${pass}, ${len}`;
   }
 }
 
-passwordOutput.innerHTML = lengthCheck();
+// passWordButton.addEventListener("click", test() {
+//   passwordOutput.innerHTML="test";
+// });
+
+passWordButton.addEventListener("click", generatePassword);
+
+// passwordOutput.innerHTML = generatePassword();
+// console.log(randomNum);
+
+// console.log(generateLength(10));
